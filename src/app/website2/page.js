d6e803d2fa4e35158e6235fa9770660c13981d02
Arpit Rajput <arpitrajput871@gmail.com>
2024-07-10 23:37:09 +0530

    routing

---
+++ src/app/website2/page.js
@@ -0,0 +1,14 @@
+import BasicTextFields from '@/component/form-component/textField'
+import React from 'react'
+
+const site1 = () => {
+    
+  return (
+    <>
+    <div>Website1</div>
+    <BasicTextFields field={'filled'}/>
+    </>
+  )
+}
+
+export default site1
